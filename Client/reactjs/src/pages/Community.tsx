680c847d6fa4488324f9e434c9b4e1ad992227c7
mhassanlodhi <lodhihassan27@gmail.com>
2026-06-30 16:31:27 +0500

    Fix ProjectCard hover menu and add additional features

--- Client/reactjs/src/pages/Community.tsx
+++ Client/reactjs/src/pages/Community.tsx
@@ -3,6 +3,7 @@ import type { Project } from '../types'
 import { use } from 'framer-motion/m'
 import { dummyGenerations } from '../assets/assets'
 import { Loader2Icon } from 'lucide-react'
+import ProjectCard from '../components/ProjectCard'
 
 const Community = () => {
 
@@ -19,6 +20,7 @@ const Community = () => {
   useEffect(()=>{
     fetchProjects()
   }, [])
+
   return loading? (
     <div className="flex items-center justify-center min-h-screen">
       <Loader2Icon className="size-7 animated-spin text-indigo-400"/>  
@@ -35,12 +37,12 @@ const Community = () => {
         {/* Projects List */}
         <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
           {projects.map((project)=>(
-            <div>{project.id}</div>
+            <ProjectCard key={project.id} gen={project} setGenerations={setProjects}
+            forCommunity={true}/>
           ))}
 
         </div>
       </div>
-
     </div>
   )
 }
